0526623afb3333c09dfd6ab37b0ed1740577fede
nmai601 <mai@weavy.com>
2021-09-13 16:11:20 -0700

    Fixed jwt comment

--- Weavy/WeavyJsInterop.cs
+++ Weavy/WeavyJsInterop.cs
@@ -30,8 +30,7 @@ namespace BlazorApp.Weavy {
         // Calling Javascript to create a new instance of Weavy via the JS Interop Module
         public async ValueTask<IJSObjectReference> Weavy(object options = null) {
             await Init();
-            // Demo JWT only for showcase.weavycloud.com
-            // Configure your JWT here when using your own weavy server
+            // Place your JWT token here
             var jwt = new { jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0ZXN0IiwiaWF0IjoxNjA1MDM1MTY2LCJleHAiOjE2MzY1NzExNzYsInN1YiI6IlRlc3QyIiwiY2xpZW50X2lkIjoiY2xpZW50aWQiLCJ1c2VybmFtZSI6IlRlc3QyIiwiZW1haWwiOiJ0ZXN0MkBlbWFpbC5jb20iLCJkaXIiOiJ3ZWF2eSJ9.1GZLZS0KEXyXGLZM1EOEgYIZYu2Gf2doyPY0trha_eI" };
             return await Bridge.InvokeAsync<IJSObjectReference>("weavy", new object[] { jwt, options });
         }
